cc55f5b4a0fd2bd75c3ea7adc0ac0cd6e9b3d829
friedchickean <kdarg29@gmail.com>
2023-10-06 03:10:51 -0600

    Category and Difficulty design upgrade

--- src/components/QuizBox.jsx
+++ src/components/QuizBox.jsx
@@ -9,6 +9,8 @@ export default function Nav() {
   // Question information
   const countdownDuration = 4;
   const [question, setQuestion] = useState('');
+  const [difficulty, setDifficulty] = useState('');
+  const [category, setCategory] = useState('');
   const [answer, setAnswer] = useState('');
   const [choices, setChoices] = useState([]);
   const [countdown, setCountdown] = useState(null);
@@ -25,6 +27,109 @@ export default function Nav() {
   const wrongSound = new Howl({
     src: [process.env.PUBLIC_URL + "/sounds/wrong.wav"],
   });
+
+  // category colours
+  const categoryStyles = {
+    "General Knowledge": {
+      bgColour: "bg-blue-600",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(3,7,4,1)]",
+    },
+    "Entertainment: Books": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3,1)]",
+    },
+    "Entertainment: Film": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3,1)]",
+    },
+    "Entertainment: Music": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3,1)]",
+    },
+    "Entertainment: Musicals & Theatres": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3,1)]",
+    },
+    "Entertainment: Television": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3,1)]",
+    },
+    "Entertainment: Video Games": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3,1)]",
+    },
+    "Entertainment: Board Games": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3,1)]",
+    },
+    "Science & Nature": {
+      bgColour: "bg-green-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,0,1)]",
+    },
+    "Science: Computers": {
+      bgColour: "bg-green-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,0,1)]",
+    },
+    "Science: Mathematics": {
+      bgColour: "bg-red-600",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(10,0,0,1)]",
+    },
+    "Mythology": {
+      bgColour: "bg-blue-300",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,1,1)]",
+    },
+    "Sports": {
+      bgColour: "bg-orange-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(3,2,0,1)]",
+    },
+    "Geography": {
+      bgColour: "bg-brown-300",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(1,1,0,1)]",
+    },
+    "History": {
+      bgColour: "bg-yellow-400",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(1,1,0,1)]",
+    },
+    "Politics": {
+      bgColour: "bg-yellow-400",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(1,1,0)]",
+    },
+    "Art": {
+      bgColour: "bg-yellow-400",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(1,1,0)]",
+    },
+    "Celebrities": {
+      bgColour: "bg-red-800",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(5,0,0)]",
+    },
+    "Animals": {
+      bgColour: "bg-green-800",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,3,0)]",
+    },
+    "Vehicles": {
+      bgColour: "bg-blue-600",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,4)]",
+    },
+    "Entertainment: Comics": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3)]",
+    },
+    "Science: Gadgets": {
+      bgColour: "bg-green-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,0)]",
+    },
+    "Entertainment: Japanese Anime & Manga": {
+      bgColour: "bg-red-700",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(5,0,0)]",
+    },
+    "Entertainment: Cartoon & Animations": {
+      bgColour: "bg-blue-500",
+      shadow: "shadow-[1px_2px_0px_1px_rgba(0,4,3)]",
+    },
+  };
+
+  const difficultyColours = {"easy": "bg-green-400", "medium": "bg-orange-400", "hard": "bg-red-400"};
+  
 
   useEffect(() => {
     fetchQuestion();
@@ -60,21 +165,26 @@ export default function Nav() {
   // Generates a new question
   const fetchQuestion = async () => {
     try {
-        const apiUrl = "https://opentdb.com/api.php";
-        const url = `${apiUrl}?amount=1&category=17`;
+      const apiUrl = "https://opentdb.com/api.php";
+      const url = `${apiUrl}?amount=1`;
 
-        const res = await fetch(`${url}`);
-        const json = await res.json();
-        const questionData = json.results[0];
+      const res = await fetch(`${url}`);
+      const json = await res.json();
+      const questionData = json.results[0];
 
-        const questionText = decodeHTMLEntities(questionData.question);
-        const correctAnswer = questionData.correct_answer;
-        const correctIndex = Math.floor(Math.random() * (questionData.incorrect_answers.length + 2));
-        let choices = [...questionData.incorrect_answers];
-        choices.splice(correctIndex, 0, correctAnswer);
-        choices = choices.map((option) => decodeHTMLEntities(option));
+      
+      const questionText = decodeHTMLEntities(questionData.question);
+      const difficulty = questionData.difficulty;
+      const category = questionData.category;
+      const correctAnswer = questionData.correct_answer;
+      const correctIndex = Math.floor(Math.random() * (questionData.incorrect_answers.length + 2));
+      let choices = [...questionData.incorrect_answers];
+      choices.splice(correctIndex, 0, correctAnswer);
+      choices = choices.map((option) => decodeHTMLEntities(option));
 
       setQuestion(questionText);
+      setDifficulty(difficulty);
+      setCategory(category);
       setChoices(choices);
       setAnswer(correctAnswer);
 
@@ -142,8 +252,11 @@ export default function Nav() {
         {/* Question information */}
         <div className="w-11/12">
           {/* Question text */}
-          <h1 className="mt-3 mb-10 text-xl font-bold text-center" id="question">{question}</h1>
-          <h2 className=""></h2>
+          <h1 className="my-3 text-xl font-bold text-center" id="question">{question}</h1>
+          <div className="flex mb-10 justify-center gap-5">
+            <p className={`px-2 rounded-sm ${categoryStyles[category].bgColour} ${categoryStyles[category].shadow}`}>{category}</p>
+            <p className={`px-2 rounded-sm ${difficultyColours[difficulty]} shadow-[1px_2px_0px_1px_rgba(0,6,7)]`}>{difficulty}</p>
+          </div>
 
           {(answer == userAnswer) && <Confetti />}
           {/* Question choices */}
@@ -158,8 +271,8 @@ export default function Nav() {
                       : userAnswer === choice
                       ? "bg-gray-500 text-white shadow-[2px_4px_0px_2px_rgba(69,69,69)]"
                       : "bg-purple-600 text-white"
-                    : "bg-purple-600 text-white hover:bg-purple-200 hover:text-black hover:shadow-[2px_4px_0px_2px_rgba(149,80,257)] hover:scale-105 cursor-pointer active:scale-95"
-                } w-10/12 py-3 px-5 my-3 mx-auto rounded-md shadow-[2px_4px_0px_2px_rgba(109,40,217)] ease-in-out duration-300`}
+                    : "bg-purple-600 text-white hover:bg-purple-200 hover:text-black hover:scale-105"
+                } w-10/12 py-3 px-5 my-3 mx-auto rounded-md shadow-[2px_4px_0px_2px_rgba(109,40,217)] cursor-pointer active:scale-95 ease-in-out duration-300`}
                 onClick={() => {
                   if (!showCorrectAnswer) {
                     handleChoiceClick(choice, index)
@@ -178,12 +291,12 @@ export default function Nav() {
 
         {/* Footer with quit button */}
         <div id="quiz-foot" className="mt-7 text-center">
-          <h2>
+          <h2 className="animate-bounce">
             {(countdown > 0) 
               ? answer == userAnswer 
                 ? "Correct! New question in " + countdown
                 : "Wrong! New question in " + countdown
-              : ""}
+              : "⠀"}
           </h2>
           <button type="button" id="quit" className="text-lg underline">
             Quit
